33bfc4b24501ed66368cf41baebe933bca991dca
jklas11 <jklas11@users.noreply.github.com>
2026-02-21 13:35:43 +0100

    Remove data from config.py
    Cleared personal data fields in the configuration.

--- config.py
+++ config.py
@@ -4,21 +4,21 @@
 URL = "https://nuernberg.termine-reservieren.de/select2?md=8"
 
 # Persönliche Daten für die Buchung
-ANREDE = "Herr" # oder "Frau"
-VORNAME = "Matthias"
-NACHNAME = "Bauer"
-EMAIL = "bauer.matt@proton.me"
-TELEFON = "017643895725"
+ANREDE = "" # oder "Frau"
+VORNAME = ""
+NACHNAME = ""
+EMAIL = ""
+TELEFON = "
 KENNZEICHEN = "" # Falls abgefragt
 
 # Adresse & Geburtsdatum
-STRASSE = "Leibnizstraße"
-HAUSNUMMER = "31"
-PLZ = "90443"
-ORT = "Nürnberg"
-GEBURTSDATUM_TAG = "23"
-GEBURTSDATUM_MONAT = "04"
-GEBURTSDATUM_JAHR = "1986"
+STRASSE = ""
+HAUSNUMMER = ""
+PLZ = ""
+ORT = ""
+GEBURTSDATUM_TAG = ""
+GEBURTSDATUM_MONAT = ""
+GEBURTSDATUM_JAHR = ""
 
 # Testmodus
 DRY_RUN = False # Wenn True, wird der "Reservieren" Button NICHT geklickt.
